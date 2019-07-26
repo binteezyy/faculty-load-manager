d0b4443cfdfdb0cd348adf8b140b3ca047750ec1
unknown <alvinpanganiban22@gmail.com>
2019-07-27 04:25:02 +0800

    Added login feature w/o css

--- faculty_load_manager/users/views.py
+++ faculty_load_manager/users/views.py
@@ -1,3 +1,69 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.http import HttpResponse, HttpResponseRedirect
 
+from django.contrib.auth import (
+    authenticate,
+    login,
+    logout
+)
+from .forms import UserLoginForm, UserRegisterForm
+
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+
+@login_required(login_url='/login/')
+def home_view(request):
+
+    context = {
+        'user': request.user,
+    }
+    return render(request, 'users/home.html', context)
+
+def login_view(request):
+    next = request.GET.get('next')
+    
+    if request.user.is_authenticated:
+        return HttpResponseRedirect(reverse('home'))
+    else:
+        form = UserLoginForm(request.POST or None)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            
+            user = authenticate(username=username, password=password)
+            login(request, user)
+
+            if next:
+                return redirect(next)
+            return HttpResponseRedirect(reverse('home'))
+
+        context = {
+            'form': form,
+        }
+        return render(request, 'users/login.html', context)
+
+def register_view(request):
+    next = request.GET.get('next')
+    form = UserRegisterForm(request.POST or None)
+    
+    if form.is_valid():
+        user = form.save(commit=False)
+        password = form.cleaned_data('password')
+        user.set_password(password)
+        user.save()
+
+        new_user = authenticate(username=user.username, password=password)
+        login(request, new_user)
+
+        if next:
+            return redirect(next)
+        return HttpResponseRedirect(reverse('home'))
+    
+    context = {
+        'form':form,
+    }
+    return render(request, 'users/register.html', context)
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('login'))
