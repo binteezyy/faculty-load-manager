0b97aec293a21f852d4f1c75acaf682422403f41
Tejada <TheoLeonard.Tejada@ingrammicro.com>
2019-08-04 22:50:39 -0700

    few typos

--- faculty_load_manager/users/urls.py
+++ faculty_load_manager/users/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('register/', views.register_view, name='register'),
     path('pload/', views.pload_view, name='pload'),
-    path('logout/', views.logout_view, name='logout')
+    path('logout/', views.logout_view, name='logout'),
 ]
--- faculty_load_manager/users/views.py
+++ faculty_load_manager/users/views.py
@@ -68,6 +68,10 @@ def register_view(request):
 
 def pload_view(request):
     
+    context = {
+        'form':form,
+        'title':"Select Loads",
+    }
     return render(request, 'components/pload.html')
 
 def logout_view(request):
