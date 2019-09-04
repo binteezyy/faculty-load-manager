9d8e90dcde4e61a078965b3379e375fa5d2d37d4
unknown <alvinpanganiban22@gmail.com>
2019-09-05 02:16:48 +0800

    requirements.txt

--- faculty_load_manager/load_manager/admin.py
+++ faculty_load_manager/load_manager/admin.py
@@ -12,25 +12,31 @@ from .models import *
 #     admin.site.register(model)
 #
 
+
 class PostTimeSelect(admin.ModelAdmin):
-    list_display = ('select_day','select_time')
+    list_display = ('select_day', 'select_time')
+
 
 class PostPreferredSchedule(admin.ModelAdmin):
-    list_display = ('user','created_at','updated_at')
+    list_display = ('user', 'created_at', 'updated_at')
     formfield_overrides = {
         models.ManyToManyField: {'widget': CheckboxSelectMultiple},
     }
 
+
 class PostSubject(admin.ModelAdmin):
-    list_display = ('subject_name', 'subject_code', 'curriculum', 'year_level', 'semester')
-    
+    list_display = ('subject_name', 'subject_code',
+                    'curriculum', 'year_level', 'semester')
+
+
 admin.site.register(Room)
 admin.site.register(Year)
 admin.site.register(Subject, PostSubject)
 admin.site.register(SchoolYear)
 admin.site.register(SemesterOffering)
 admin.site.register(SectionOffering)
-admin.site.register(PreferredTime,PostTimeSelect)
-admin.site.register(PreferredSchedule,PostPreferredSchedule)
+admin.site.register(PreferredTime, PostTimeSelect)
+admin.site.register(PreferredSchedule, PostPreferredSchedule)
 admin.site.register(BlockSection)
+admin.site.register(Curriculum)
 # Register your models here.
