a106d6f0dc37e3d109e53eca34d4ad778365a98a
Joshua Pascual <joshuajohnpascual@gmail.com>
2019-09-05 16:49:46 +0800

    Django-extensions

--- faculty_load_manager/faculty_load_manager/settings.py
+++ faculty_load_manager/faculty_load_manager/settings.py
@@ -33,6 +33,7 @@ LOGIN_URL = ''
 # Application definition
 
 INSTALLED_APPS = [
+    'django_extensions',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
