5c0c828feb0c4a063965e5c921284cc40aebbb12
unknown <alvinpanganiban22@gmail.com>
2019-10-09 15:35:29 +0800

    few modifications to generate secOff

--- faculty_load_manager/load_manager/views.py
+++ faculty_load_manager/load_manager/views.py
@@ -493,107 +493,136 @@ def generate_semester_offering(request):
     return HttpResponse(semOff.subject.all())
 
 def generate_section_offering(request):
-    curriculum = Curriculum.objects.get(curriculum='1112')
-    semester = 0
-    school_year = SchoolYear.objects.latest('pk')
+    settings = Setting.objects.all()[0]
+    semester = str(settings.semester)
+    start_year = str(settings.school_year.start_year)
+    end_year = str(settings.school_year.end_year)
+
     try:
-        fifth_count = BlockSection.objects.filter(school_year=school_year, semester=semester, year_level=5).count()
+        start = Year.objects.get(year=start_year)
+    except Year.DoesNotExist:
+        new_year = Year(year=start_year)
+        new_year.save()
+        start = Year.objects.get(year=start_year)
+
+    try:
+        end = Year.objects.get(year=end_year)
+    except Year.DoesNotExist:
+        new_year = Year(year=end_year)
+        new_year.save()
+        end  = Year.objects.get(year=end_year)
+
+    try:
+        sy = SchoolYear.objects.get(start_year=start, end_year=end)
+    except SchoolYear.DoesNotExist:
+        new_sy = SchoolYear(start_year=start, end_year=end)
+        new_sy.save()
+        sy = SchoolYear.objects.get(start_year=start, end_year=end)
+
+    try:
+        fifth_count = BlockSection.objects.filter(school_year=sy, semester=semester, year_level=5).count()
     except:
         fifth_count = 0
     print(f'fifth year - {fifth_count}')
     try:
-        fourth_count = BlockSection.objects.filter(school_year=school_year, semester=semester, year_level=4).count()
+        fourth_count = BlockSection.objects.filter(school_year=sy, semester=semester, year_level=4).count()
     except:
         fourth_count = 0
     print(f'fourth year - {fourth_count}')
     try:
-        third_count = BlockSection.objects.filter(school_year=school_year, semester=semester, year_level=3).count()
+        third_count = BlockSection.objects.filter(school_year=sy, semester=semester, year_level=3).count()
     except:
         third_count = 0
     print(f'third year - {third_count}')
     try:
-        second_count = BlockSection.objects.filter(school_year=school_year, semester=semester, year_level=2).count()
+        second_count = BlockSection.objects.filter(school_year=sy, semester=semester, year_level=2).count()
     except:
         second_count = 0
     print(f'second year - {second_count}')
     try:
-        first_count = BlockSection.objects.filter(school_year=school_year, semester=semester, year_level=1).count()
+        first_count = BlockSection.objects.filter(school_year=sy, semester=semester, year_level=1).count()
     except:
         first_count = 0
     print(f'first year - {first_count}')
 
-    semOff = SemesterOffering.objects.get(school_year=school_year, semester=semester)
-    first_s = Subject.objects.filter(year_level=1, semester=semester, curriculum=curriculum).filter(
-            Q(subject_code__startswith='COEN')|Q(subject_code__startswith='BSCOE'))
-    second_s = Subject.objects.filter(year_level=2, semester=semester, curriculum=curriculum).filter(
-            Q(subject_code__startswith='COEN')|Q(subject_code__startswith='BSCOE'))
-    third_s = Subject.objects.filter(year_level=3, semester=semester, curriculum=curriculum).filter(
-            Q(subject_code__startswith='COEN')|Q(subject_code__startswith='BSCOE'))
-    fourth_s = Subject.objects.filter(year_level=4, semester=semester, curriculum=curriculum).filter(
-            Q(subject_code__startswith='COEN')|Q(subject_code__startswith='BSCOE'))
-    fifth_s = Subject.objects.filter(year_level=5, semester=semester, curriculum=curriculum).filter(
-            Q(subject_code__startswith='COEN')|Q(subject_code__startswith='BSCOE'))
+    semOff = SemesterOffering.objects.get(school_year=sy, semester=semester)
+    # first_s = Subject.objects.filter(year_level=1, semester=semester, curriculum=first_c).filter(
+    #         Q(subject_code__startswith='COEN')|Q(subject_code__startswith='BSCOE'), thesis_flag=False)
+    # second_s = Subject.objects.filter(year_level=2, semester=semester, curriculum=second_c).filter(
+    #         Q(subject_code__startswith='COEN')|Q(subject_code__startswith='BSCOE'), thesis_flag=False)
+    # third_s = Subject.objects.filter(year_level=3, semester=semester, curriculum=third_c).filter(
+    #         Q(subject_code__startswith='COEN')|Q(subject_code__startswith='BSCOE'), thesis_flag=False)
+    # fourth_s = Subject.objects.filter(year_level=4, semester=semester, curriculum=fourth_c).filter(
+    #         Q(subject_code__startswith='COEN')|Q(subject_code__startswith='BSCOE'), thesis_flag=False)
+    # fifth_s = Subject.objects.filter(year_level=5, semester=semester, curriculum=fifth_c).filter(
+    #         Q(subject_code__startswith='COEN')|Q(subject_code__startswith='BSCOE'), thesis_flag=False)
+    first_s = semOff.subject.all().filter(year_level=1)
+    second_s = semOff.subject.all().filter(year_level=2)
+    third_s = semOff.subject.all().filter(year_level=3)
+    fourth_s = semOff.subject.all().filter(year_level=4)
+    fifth_s = semOff.subject.all().filter(year_level=5)
+
     if fifth_count > 0 and fifth_s.count() > 0:
         for i in range(fifth_count):
-            bs = BlockSection.objects.filter(school_year=school_year, semester=semester, year_level=5)[i]
+            bs = BlockSection.objects.filter(school_year=sy, semester=semester, year_level=5)[i]
             for j in range(fifth_s.count()):
                 try:
-                    new_secOff = SectionOffering.objects.get(school_year=school_year, semester=semester, subject=fifth_s[j],
+                    new_secOff = SectionOffering.objects.get(school_year=sy, semester=semester, subject=fifth_s[j],
                     block_section=bs)
                 except SectionOffering.DoesNotExist:
-                    new_secOff = SectionOffering(school_year=school_year, semester=semester, subject=fifth_s[j],
+                    new_secOff = SectionOffering(school_year=sy, semester=semester, subject=fifth_s[j],
                     block_section=bs)
                     new_secOff.save()
                 print(new_secOff)
 
     if fourth_count > 0 and fourth_s.count() > 0:
         for i in range(fourth_count):
-            bs = BlockSection.objects.filter(school_year=school_year, semester=semester, year_level=4)[i]
+            bs = BlockSection.objects.filter(school_year=sy, semester=semester, year_level=4)[i]
             for j in range(fourth_s.count()):
                 try:
-                    new_secOff = SectionOffering.objects.get(school_year=school_year, semester=semester, subject=fourth_s[j],
+                    new_secOff = SectionOffering.objects.get(school_year=sy, semester=semester, subject=fourth_s[j],
                     block_section=bs)
                 except SectionOffering.DoesNotExist:
-                    new_secOff = SectionOffering(school_year=school_year, semester=semester, subject=fourth_s[j],
+                    new_secOff = SectionOffering(school_year=sy, semester=semester, subject=fourth_s[j],
                     block_section=bs)
                     new_secOff.save()
                 print(new_secOff)
 
     if third_count > 0 and third_s.count() > 0:
         for i in range(third_count):
-            bs = BlockSection.objects.filter(school_year=school_year, semester=semester, year_level=3)[i]
+            bs = BlockSection.objects.filter(school_year=sy, semester=semester, year_level=3)[i]
             for j in range(third_s.count()):
                 try:
-                    new_secOff = SectionOffering.objects.get(school_year=school_year, semester=semester, subject=third_s[j],
+                    new_secOff = SectionOffering.objects.get(school_year=sy, semester=semester, subject=third_s[j],
                     block_section=bs)
                 except SectionOffering.DoesNotExist:
-                    new_secOff = SectionOffering(school_year=school_year, semester=semester, subject=third_s[j],
+                    new_secOff = SectionOffering(school_year=sy, semester=semester, subject=third_s[j],
                     block_section=bs)
                     new_secOff.save()
                 print(new_secOff)
 
     if second_count > 0 and second_s.count() > 0:
         for i in range(second_count):
-            bs = BlockSection.objects.filter(school_year=school_year, semester=semester, year_level=2)[i]
+            bs = BlockSection.objects.filter(school_year=sy, semester=semester, year_level=2)[i]
             for j in range(second_s.count()):
                 try:
-                    new_secOff = SectionOffering.objects.get(school_year=school_year, semester=semester, subject=second_s[j],
+                    new_secOff = SectionOffering.objects.get(school_year=sy, semester=semester, subject=second_s[j],
                     block_section=bs)
                 except SectionOffering.DoesNotExist:
-                    new_secOff = SectionOffering(school_year=school_year, semester=semester, subject=second_s[j],
+                    new_secOff = SectionOffering(school_year=sy, semester=semester, subject=second_s[j],
                     block_section=bs)
                     new_secOff.save()
                 print(new_secOff)
 
     if first_count > 0 and first_s.count() > 0:
         for i in range(first_count):
-            bs = BlockSection.objects.filter(school_year=school_year, semester=semester, year_level=1)[i]
+            bs = BlockSection.objects.filter(school_year=sy, semester=semester, year_level=1)[i]
             for j in range(first_s.count()):
                 try:
-                    new_secOff = SectionOffering.objects.get(school_year=school_year, semester=semester, subject=first_s[j],
+                    new_secOff = SectionOffering.objects.get(school_year=sy, semester=semester, subject=first_s[j],
                     block_section=bs)
                 except SectionOffering.DoesNotExist:
-                    new_secOff = SectionOffering(school_year=school_year, semester=semester, subject=first_s[j],
+                    new_secOff = SectionOffering(school_year=sy, semester=semester, subject=first_s[j],
                     block_section=bs)
                     new_secOff.save()
                 print(new_secOff)
