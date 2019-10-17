576c28394427beb1304c67641aa49e37934b76f8
Theo Leonard P. Tejada <35455761+kkshinichi@users.noreply.github.com>
2019-10-17 14:39:48 +0800

    added instruction tooltip to preferred time table

--- faculty_load_manager/static/js/ploadscript.js
+++ faculty_load_manager/static/js/ploadscript.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    $('[data-toggle="tooltip"]').tooltip();
     $("table").each(function() { // invert tr and td
         var $this = $(this);
         var newrows = [];
