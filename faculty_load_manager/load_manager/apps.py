e5d874a83297de00d7a089359620a3783ea65ae5
Joshua <jjspscl@gmail.com>
2020-02-24 10:10:21 +0800

    staging up

--- faculty_load_manager/load_manager/apps.py
+++ faculty_load_manager/load_manager/apps.py
@@ -62,50 +62,50 @@ def ss():
 def startup():
     from .models import Year,SchoolYear,PreferredTime,DAY_OF_THE_WEEK
     from django.utils import timezone
-    # ss()
-    # YEAR
-    # for i in range(1904,int(timezone.now().strftime("%Y")) +3):
-    #     Year.objects.get_or_create(year=i)
-    #
-    #
-    # print("SCHOOL YEAR")
-    # try:
-    #     current = SchoolYear.objects.get_or_create(
-    #                             start_year= Year.objects.get(year = int(timezone.now().strftime("%Y"))) ,
-    #                             end_year= Year.objects.get(year = int(timezone.now().strftime("%Y")) +1)
-    #                             )
-    #     print(int(timezone.now().strftime("%Y"))+2)
-    #     next = SchoolYear.objects.get_or_create(
-    #                             start_year= Year.objects.get(year = int(timezone.now().strftime("%Y"))+1) ,
-    #                             end_year= Year.objects.get(year = int(timezone.now().strftime("%Y")) +2)
-    #                             )
-    #
-    # except Exception as e:
-    #     raise e
-    #
-    # try:
-    #     if PreferredTime.objects.all().count() <= 0:
-    #         for x,day in DAY_OF_THE_WEEK():
-    #             for y, day in PreferredTime.TIME_SELECT:
-    #                 try:
-    #                     sched = PreferredTime.objects.get(
-    #                     select_day = x,
-    #                     select_time = y
-    #                     )
-    #                     print(f'{x} {y} already exists')
-    #                 except PreferredTime.DoesNotExist:
-    #                     sched = PreferredTime(
-    #                         select_day = x,
-    #                         select_time = y
-    #                     )
-    #                     sched.save()
-    #         print("PREFERRED TIME INITIALIZED")
-    # except Exception as e:
-    #     PreferredTime.objects.all().delete()
-    #     raise e
+    ss()
+    YEAR
+    for i in range(1904,int(timezone.now().strftime("%Y")) +3):
+        Year.objects.get_or_create(year=i)
+
+
+    print("SCHOOL YEAR")
+    try:
+        current = SchoolYear.objects.get_or_create(
+                                start_year= Year.objects.get(year = int(timezone.now().strftime("%Y"))) ,
+                                end_year= Year.objects.get(year = int(timezone.now().strftime("%Y")) +1)
+                                )
+        print(int(timezone.now().strftime("%Y"))+2)
+        next = SchoolYear.objects.get_or_create(
+                                start_year= Year.objects.get(year = int(timezone.now().strftime("%Y"))+1) ,
+                                end_year= Year.objects.get(year = int(timezone.now().strftime("%Y")) +2)
+                                )
+
+    except Exception as e:
+        raise e
+
+    try:
+        if PreferredTime.objects.all().count() <= 0:
+            for x,day in DAY_OF_THE_WEEK():
+                for y, day in PreferredTime.TIME_SELECT:
+                    try:
+                        sched = PreferredTime.objects.get(
+                        select_day = x,
+                        select_time = y
+                        )
+                        print(f'{x} {y} already exists')
+                    except PreferredTime.DoesNotExist:
+                        sched = PreferredTime(
+                            select_day = x,
+                            select_time = y
+                        )
+                        sched.save()
+            print("PREFERRED TIME INITIALIZED")
+    except Exception as e:
+        PreferredTime.objects.all().delete()
+        raise e
 class LoadManagerConfig(AppConfig):
     name = 'load_manager'
 
     def ready(request):
         print("READY")
-        # startup()
+        startup()
